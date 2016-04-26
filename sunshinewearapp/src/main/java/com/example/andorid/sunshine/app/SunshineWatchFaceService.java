3b6964fe915fcb33759b2b3db908b419233c3525
xnml <dragoonart@mail.bg>
2016-04-26 11:16:33 +0300

    comment out class, to avoid confusion

--- sunshinewearapp/src/main/java/com/example/andorid/sunshine/app/SunshineWatchFaceService.java
+++ sunshinewearapp/src/main/java/com/example/andorid/sunshine/app/SunshineWatchFaceService.java
@@ -10,61 +10,62 @@ import android.view.SurfaceHolder;
 /**
  * Created by DragooNArT-PC on 4/24/2016.
  */
-public class SunshineWatchFaceService extends CanvasWatchFaceService {
-
-
-    @Override
-    public Engine onCreateEngine() {
-        return new Engine();
-    }
-
-
-    private class Engine extends CanvasWatchFaceService.Engine {
-
-        private boolean mLowBitAmbient;
-
-
-        @Override
-        public void onCreate(SurfaceHolder holder) {
-            super.onCreate(holder);
-
-            setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFaceService.this)
-                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
-                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
-                    .setShowSystemUiTime(true)
-                    .build());
-
-            //TODO resource alloc
-        }
-
-        @Override
-        public void onTimeTick() {
-            super.onTimeTick();
-            invalidate();
-        }
-
-        @Override
-        public void onDraw(Canvas canvas, Rect bounds) {
-            super.onDraw(canvas, bounds);
-        }
-
-        @Override
-        public void onPropertiesChanged(Bundle properties) {
-            super.onPropertiesChanged(properties);
-            //if it's needed
-            //boolean burnInProtection = properties.getBoolean(PROPERTY_BURN_IN_PROTECTION,false);
-            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT);
-        }
-
-        @Override
-        public void onAmbientModeChanged(boolean inAmbientMode) {
-            super.onAmbientModeChanged(inAmbientMode);
-
-            if(inAmbientMode) {
-                //TODO stuff ambient
-            }
-
-            invalidate();//refresh UI
-        }
-    }
-}
+// not using this check out MainSunshineWearActivity.java
+//public class SunshineWatchFaceService extends CanvasWatchFaceService {
+//
+//
+//    @Override
+//    public Engine onCreateEngine() {
+//        return new Engine();
+//    }
+//
+//
+//    private class Engine extends CanvasWatchFaceService.Engine {
+//
+//        private boolean mLowBitAmbient;
+//
+//
+//        @Override
+//        public void onCreate(SurfaceHolder holder) {
+//            super.onCreate(holder);
+//
+//            setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFaceService.this)
+//                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
+//                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+//                    .setShowSystemUiTime(true)
+//                    .build());
+//
+//            //TODO resource alloc
+//        }
+//
+//        @Override
+//        public void onTimeTick() {
+//            super.onTimeTick();
+//            invalidate();
+//        }
+//
+//        @Override
+//        public void onDraw(Canvas canvas, Rect bounds) {
+//            super.onDraw(canvas, bounds);
+//        }
+//
+//        @Override
+//        public void onPropertiesChanged(Bundle properties) {
+//            super.onPropertiesChanged(properties);
+//            //if it's needed
+//            //boolean burnInProtection = properties.getBoolean(PROPERTY_BURN_IN_PROTECTION,false);
+//            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT);
+//        }
+//
+//        @Override
+//        public void onAmbientModeChanged(boolean inAmbientMode) {
+//            super.onAmbientModeChanged(inAmbientMode);
+//
+//            if(inAmbientMode) {
+//                //TODO stuff ambient
+//            }
+//
+//            invalidate();//refresh UI
+//        }
+//    }
+//}
